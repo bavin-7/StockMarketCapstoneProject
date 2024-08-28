56d35da7490d84425fbd2a80caddf9d7b079d748
Bavin Kuniyamkottil <145380161+bavin-7@users.noreply.github.com>
2024-08-28 15:50:24 +0530

    Delete batch4-team6-wealth-vue-investment-tracker-master directory

--- batch4-team6-wealth-vue-investment-tracker-master/back-end/portfolio/src/main/java/com/capstone/project/samplePortfolio/repository/PortfolioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.project.samplePortfolio.repository;
-
-import com.capstone.project.samplePortfolio.entity.Portfolio;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PortfolioRepository extends MongoRepository<Portfolio,String> {
-}
